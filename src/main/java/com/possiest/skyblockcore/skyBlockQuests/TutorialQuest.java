ff1dba26a7c563ffc837458b3f8f90fe0f91a522
Mathias <pxlib@proton.me>
2023-01-25 17:00:54 +0100

    Tutorial Quests

---
+++ src/main/java/com/possiest/skyblockcore/skyBlockQuests/TutorialQuest.java
@@ -0,0 +1,10 @@
+package com.possiest.skyblockcore.skyBlockQuests;
+
+import com.possiest.skyblockcore.managers.quests.QuestSystem;
+import org.bukkit.event.Listener;
+
+public class TutorialQuest implements Listener {
+    QuestSystem questSystem = new QuestSystem();
+
+
+}
